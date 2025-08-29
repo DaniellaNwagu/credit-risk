9ea3021841e785c86d473d3f5ceaf09846bce887
DaniellaNwagu <nwagudaniellaakachi@gmail.com>
2025-08-29 14:38:45 +0100

    the initial commit just to setup the projet on github

---
+++ creditrisk/creditrisk/src/main/java/com/cbfacademy/creditrisk/CreditriskApplication.java
@@ -0,0 +1,13 @@
+package com.cbfacademy.creditrisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditriskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditriskApplication.class, args);
+	}
+
+}
---
+++ creditrisk/src/test/java/com/cbfacademy/creditrisk/CreditriskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbfacademy.creditrisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditriskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
